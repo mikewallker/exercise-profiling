eb137257d7ab3ff597852713e25097882a55ea76
joseph <yosiajosephc@gmail.com>
2025-03-14 19:59:22 +0700

    [Refactoring] - Optimizing method getAllStudentWithCourse

--- src/main/java/com/advpro/profiling/tutorial/service/StudentService.java
+++ src/main/java/com/advpro/profiling/tutorial/service/StudentService.java
@@ -24,19 +24,14 @@ public class StudentService {
     private StudentCourseRepository studentCourseRepository;
 
     public List<StudentCourse> getAllStudentsWithCourses() {
-        List<Student> students = studentRepository.findAll();
-        List<StudentCourse> studentCourses = new ArrayList<>();
-        for (Student student : students) {
-            List<StudentCourse> studentCoursesByStudent = studentCourseRepository.findByStudentId(student.getId());
-            for (StudentCourse studentCourseByStudent : studentCoursesByStudent) {
-                StudentCourse studentCourse = new StudentCourse();
-                studentCourse.setStudent(student);
-                studentCourse.setCourse(studentCourseByStudent.getCourse());
-                studentCourses.add(studentCourse);
-            }
+        List<StudentCourse> studentCourses = studentCourseRepository.findAllWithStudents();
+        for (StudentCourse studentCourse : studentCourses) {
+            studentCourse.setStudent(studentCourse.getStudent());
+            studentCourse.setCourse(studentCourse.getCourse());
         }
         return studentCourses;
     }
+
 
     public Optional<Student> findStudentWithHighestGpa() {
         List<Student> students = studentRepository.findAll();
